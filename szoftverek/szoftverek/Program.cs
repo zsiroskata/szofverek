3db939748d4bffacb2dc0369600a92889d4b9d66
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2023-11-05 19:10:20 +0100

    Add files via upload
    :)))))))))))))))))))))))))))

---
+++ szoftverek/szoftverek/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Threading.Channels;
+
+namespace szoftverek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("5.feladat");
+            List<Szoftver> szofverek = new();
+            var sr = new StreamReader(
+                path: @"..\..\..\src\szoftverek.txt",
+                encoding: System.Text.Encoding.UTF8
+                );
+
+            while (!sr.EndOfStream)
+            {
+                sr.ReadLine();
+                var sor = new Szoftver(sr.ReadLine());
+                szofverek.Add(sor);
+                
+            }
+
+            Console.WriteLine("6. feladat");
+            for (int i = 0; i < szofverek.Count; i++)
+            {
+                Console.WriteLine(szofverek[i]);
+            }
+
+            Console.WriteLine("\n7.feladat");
+            Console.WriteLine($"Összesen:{antiFugg(szofverek)}db");
+
+            Console.WriteLine("\n8.feladat");
+            //nincs
+
+            Console.WriteLine("\n9.feladat");
+
+
+            //        ????
+            //      ?     ?
+            //           ?
+            //          ?         
+            //          ?         
+            //          ■
+            
+            foreach (var s in szofverek)
+            {
+                
+                Console.WriteLine($"{s.brutto()}");
+            }
+            
+            //- - - - - - - - main vége - - - - - - - -
+        }
+
+        static string antiFugg(List<Szoftver> szoftvers)
+        {
+            double ert =8.5;
+            List<Szoftver> antilista = new();
+          
+            foreach (var gep in szoftvers)
+            {
+                if (gep.Ertekeles > ert && gep.Kategoria == "antivírus")
+                {
+                   antilista.Add(gep);
+                }
+            }
+            return $"{antilista.Count}";
+        }
+
+
+
+    }
+}
---
+++ szoftverek/szoftverek/Szoftver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szoftverek
+{
+    internal class Szoftver
+    {
+        public int Azonosíto { get; set; }
+        public string NevEsVerzio{ get; set; }
+        public bool Licenc { get; set; }
+        public string Oprendszer { get; set; }
+        public string Kategoria { get; set; }
+        public double Ertekeles { get; set; }
+        public double Netto { get; set; }
+        public int Adokulcs { get; set; }
+        
+        public Szoftver(string sor) {
+            string[] adat = sor.Split("|");
+            Azonosíto = int.Parse(adat[0]);
+            NevEsVerzio = adat[1];
+            Licenc = adat[2] == "fizetős";
+            Oprendszer = adat[3];
+            Kategoria = adat[4];
+            Ertekeles = Convert.ToDouble(adat[5]);
+            Netto = Convert.ToDouble(adat[6]);
+            Adokulcs = int.Parse(adat[7]);
+            Adokulcs = int.Parse(adat[7]);
+        }
+        public override string ToString()
+        {
+            return $"Azonosító: {Azonosíto}, név és verziószám: {NevEsVerzio}, licenc típus: {Licenc}, operációs rendszerek: {Oprendszer}, kategória: {Kategoria}, felhasználói értékelés: {Ertekeles}, nettó ár: {Netto}, adókulcs: {Adokulcs}";
+        }
+
+        public double brutto()
+        {
+            return Netto * (Adokulcs / 100);
+        }
+
+    }
+}
